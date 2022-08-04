0bf267f33adaa79c5edfabffb85ee411795ce08d
emilyenegrette <emilyenegrette@gmail.com>
2022-08-04 17:15:10 -0400

    cambios en main.js

--- src/test.js
+++ src/test.js
@@ -1,2 +1,13 @@
 import {harrySpecies, filterHouses, sortAtoZ} from './data.js'
 
+const allCharacters =[
+{
+
+}
+
+describe('sortAtoZ return elementos ordenados alfabéticamente de la A a la Z', () => {
+    it('is a function', () => {
+      expect(typeof sortAtoZ).toBe('function');
+    });
+  
+]
